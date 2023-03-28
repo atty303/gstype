d39a39c310efaebc1e026d3a2169069b2b155cd1
Koji AGAWA <atty303@gmail.com>
2023-03-28 21:30:06 +0900

    feat: exit

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,78 @@
-use wayland_client::{Connection, Dispatch, protocol::{wl_registry, wl_seat}, QueueHandle, WEnum};
-use protocol::gamescope_input_method_manager::GamescopeInputMethodManager;
+use wayland_client::{Connection, Dispatch, QueueHandle};
+use wayland_client::protocol::{wl_registry, wl_seat};
+
 use protocol::gamescope_input_method;
-use crate::protocol::gamescope_input_method_manager;
+use protocol::gamescope_input_method_manager;
 
 mod protocol;
 
+#[derive(Clone)]
 struct AppData {
+    text: String,
+}
+
+struct AppState {
+    running: bool,
     seat: Option<wl_seat::WlSeat>,
-    imm: Option<GamescopeInputMethodManager>,
+    imm: Option<gamescope_input_method_manager::GamescopeInputMethodManager>,
     im: Option<gamescope_input_method::GamescopeInputMethod>,
 }
 
-impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
+impl AppState {
+    fn create_im(&mut self, qh: &QueueHandle<AppState>, data: &AppData) {
+        if let (Some(seat), Some(imm)) = (&self.seat, &self.imm) {
+            if self.im.is_none() {
+                self.im = Some(imm.create_input_method(&seat, qh, data.clone()))
+            }
+        }
+    }
+}
+
+impl Dispatch<wl_registry::WlRegistry, AppData> for AppState {
     fn event(
         state: &mut Self,
         registry: &wl_registry::WlRegistry,
         event: wl_registry::Event,
-        _: &(),
-        _conn: &Connection,
-        qh: &QueueHandle<AppData>,
+        data: &AppData,
+        _: &Connection,
+        qh: &QueueHandle<Self>,
     ) {
-        // When receiving events from the wl_registry, we are only interested in the
-        // `global` event, which signals a new available global.
-        // When receiving this event, we just print its characteristics in this example.
         if let wl_registry::Event::Global { name, interface, version } = event {
-            println!("[{}] {} (v{})", name, interface, version);
             match &interface[..] {
                 "wl_seat" => {
-                    registry.bind::<wl_seat::WlSeat, _, _>(name, version, qh, ());
+                    registry.bind::<wl_seat::WlSeat, _, _>(name, version, qh, data.clone());
                 }
                 "gamescope_input_method_manager" => {
-                    let imm = registry.bind::<GamescopeInputMethodManager, _, _>(name, version, qh, ());
+                    let imm = registry.bind::<gamescope_input_method_manager::GamescopeInputMethodManager, _, _>(name, version, qh, ());
                     state.imm = Some(imm);
-                    //protocol::gamescope_input_method_manager::GamescopeInputMethodManager::
+                    state.create_im(qh, data);
                 }
                 _ => {}
             }
-            //if protocol::gamescope_input_method_manager::GamescopeInputMethodManager::
-        } else {
-            println!("{:?}", event);
         }
     }
 }
 
-impl Dispatch<wl_seat::WlSeat, ()> for AppData {
+impl Dispatch<wl_seat::WlSeat, AppData> for AppState {
     fn event(
         state: &mut Self,
         seat: &wl_seat::WlSeat,
         event: wl_seat::Event,
-        _: &(),
+        data: &AppData,
         _: &Connection,
         qh: &QueueHandle<Self>,
     ) {
-        if let wl_seat::Event::Name { name } = event {
-            println!("wl_seat::name {:?}", name);
+        if let wl_seat::Event::Name { name: _ } = event {
             state.seat = Some(seat.clone());
-
-            if let Some(imm) = state.imm.clone() {
-                let r = imm.create_input_method(&seat, qh, ());
-                println!("im: {:?}", r);
-                state.im = Some(r);
-            }
+            state.create_im(qh, data);
         }
     }
 }
 
-impl Dispatch<GamescopeInputMethodManager, ()> for AppData {
+impl Dispatch<gamescope_input_method_manager::GamescopeInputMethodManager, ()> for AppState {
     fn event(
         _: &mut Self,
-        _: &GamescopeInputMethodManager,
+        _: &gamescope_input_method_manager::GamescopeInputMethodManager,
         _: gamescope_input_method_manager::Event,
         _: &(),
         _: &Connection,
@@ -77,41 +81,39 @@ impl Dispatch<GamescopeInputMethodManager, ()> for AppData {
     }
 }
 
-impl Dispatch<gamescope_input_method::GamescopeInputMethod, ()> for AppData {
+impl Dispatch<gamescope_input_method::GamescopeInputMethod, AppData> for AppState {
     fn event(
         state: &mut Self,
         _: &gamescope_input_method::GamescopeInputMethod,
         event: gamescope_input_method::Event,
-        _: &(),
+        data: &AppData,
         _: &Connection,
         _: &QueueHandle<Self>,
     ) {
-        println!("GamescopeInputMethod: {:?}", event);
-        if let gamescope_input_method::Event::Done { serial } = event {
-            if serial == 1 {
-                if let Some(im) = &state.im {
-                    im.set_string("Hello, world! こんにちは、世界！".to_string());
-                    im.commit(serial);
-                }
+        if let (gamescope_input_method::Event::Done { serial }, Some(im)) = (event, &state.im) {
+            if state.running {
+                im.set_string(data.text.clone());
+                im.commit(serial);
+                state.running = false;
             }
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-    let conn = Connection::connect_to_env().unwrap();
-    let display = conn.display();
+    let text = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+
+    let conn = Connection::connect_to_env().expect("Failed to connect to Gamescope");
 
     let mut event_queue = conn.new_event_queue();
     let qh = event_queue.handle();
 
-    let _registry = display.get_registry(&qh, ());
+    let display = conn.display();
+    display.get_registry(&qh, AppData { text });
 
-    //event_queue.roundtrip(&mut AppData).unwrap();
-
-    let mut state = AppData { seat: None, imm: None, im: None };
-    loop {
+    let mut state = AppState { running: true, seat: None, imm: None, im: None };
+    while state.running {
         event_queue.blocking_dispatch(&mut state).unwrap();
     }
+    event_queue.roundtrip(&mut state).unwrap();
 }
